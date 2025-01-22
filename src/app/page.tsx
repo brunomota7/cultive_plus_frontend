2279cb2280502912eab3a6d1b3243a35b9d0c80b
Bruno Mota <bruno7motadev@gmail.com>
2025-01-22 19:00:55 -0300

    feat: Criado O HEADER e os componentes ABOUT US e SERVICES da landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import AboutUs from "@/components/landingpage/AboutUs";
 import ImgHeaderBg from "../../public/images/fundoHeader.svg";
 import ImgLogo from "../../public/images/logo.png";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Service from "@/components/landingpage/Services";
 
 export default function LandingPage() {
   const itemsNavbar = [
@@ -10,7 +14,8 @@ export default function LandingPage() {
   ];
 
   return (
-    <main className="w-full flex flex-col">
+    <div className="w-full flex flex-col items-center justify-center">
+      {/* Header */}
       <header className="relative w-full h-[650px] flex items-center justify-center bg-[#438f55] overflow-hidden">
         <Image 
           src={ImgHeaderBg}
@@ -18,16 +23,19 @@ export default function LandingPage() {
           className="absolute w-[80%] h-[600px] object-cover rounded-2xl"
         />
 
+        {/* Section Navabar */}
         <section className="absolute top-10 w-3/5 h-14 px-8 flex items-center justify-between bg-black/60 rounded-lg">
           <a
             href="#"
             className="flex items-center"> {/* Logo Image */}
-            <Image 
-              src={ImgLogo}
-              alt="Logo Cultive+"
-              width={40}
-              height={40}
-            />
+            <AspectRatio ratio={16 / 9}>
+              <Image 
+                src={ImgLogo}
+                alt="Logo Cultive+"
+                width={40}
+                height={40}
+              />
+            </AspectRatio>
             <h2 className="ml-2 flex items-center font-extrabold text-xl text-white">Cultive<span className="font-extrabold text-green-500">+</span></h2>
           </a>
 
@@ -49,14 +57,18 @@ export default function LandingPage() {
           </nav>
         </section>
 
-        <section className="absolute bottom-20 left-64 w-44 h-12 ml-2 flex"> {/* Cadastre-se Btn */}
-          <a 
-            href="#"
-            className="w-full h-full flex items-center justify-center text-lg font-bold text-white rounded-lg shadow-xl bg-amber-400 hover:bg-amber-500 duration-300 ease-in-out">
+        {/* Cadastre-se Btn */}
+        <section className="absolute bottom-20 left-64 w-44 h-12 ml-2 flex"> 
+          <Button className="w-full h-full flex items-center justify-center text-lg font-bold text-white rounded-lg shadow-xl bg-amber-400 hover:bg-amber-500 duration-300 ease-in-out">
             Cadastre-se
-          </a>
+          </Button>
         </section>
       </header>
-    </main>
+
+      <main className="w-full flex flex-col items-center justify-center bg-slate-100">
+        <AboutUs />
+        <Service />
+      </main>
+    </div>
   );
 }
---
+++ src/components/landingpage/Services/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { BarChartBig, CloudSunRain, Handshake, Rocket } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import { useEffect, useState } from "react";
+
+const services = [
+    { title: 'Conectamos você a compradores', description: 'Ajudamos a compradores chegar até você e adquirir seus produtos.', icon: <Handshake className="w-12 h-12 text-amber-400" /> },
+    { title: 'Dados estatísticos', description: 'Geramos dados estatísticos para você baseados em seus plantios ou colheitas.', icon: <BarChartBig className="w-12 h-12 text-amber-400" /> },
+    { title: 'Alertas Climáticos', description: 'A cultive te informa sobre o clima e qual soluções para evitar percas.', icon: <CloudSunRain className="w-12 h-12 text-amber-400" /> },
+    { title: 'Gerenciamento', description: 'Ajudaremos você a gerenciar seu negócio e assim crescer cada vez mais.', icon: <Rocket className="w-12 h-12 text-amber-400" /> },
+];
+
+export default function Service() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex === services.length - 1 ? 0 : prevIndex + 1));
+        }, 5000); // Muda a cada 5 segundos
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="w-full p-4 flex flex-col items-center justify-center bg-[#438f55]">
+            <main className="w-4/5 h-[300px] flex flex-col items-center justify-center bg-[#0b5d43] ">
+                <h2 className="text-4xl font-extrabold text-white ml-2">Nossos Serviços</h2>
+                <section className="relative w-full flex items-center justify-center">
+                    <AnimatePresence>
+                        <motion.div
+                            key={currentIndex}
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.7 }}
+                            className="w-full flex flex-col items-center text-center"
+                        >
+                            <div className="p-6 flex flex-col items-center">
+                                <div className="flex items-center gap-4">
+                                    <div className="mb-4 sm:mb-6">
+                                        {services[currentIndex].icon}
+                                    </div>
+                                    <h3 className="text-white text-xl sm:text-2xl font-bold mb-2 sm:mb-4">{services[currentIndex].title}</h3>
+                                </div>
+                                <p className="text-white text-base sm:text-lg font-light leading-relaxed max-w-2xl px-4 sm:px-0">
+                                    {services[currentIndex].description}
+                                </p>
+                            </div>
+                        </motion.div>
+                    </AnimatePresence>
+                </section>
+                <div>
+                    <div className="mt-2 sm:mt-4 flex items-center justify-center space-x-2 sm:space-x-4">
+                        {services.map((_, index) => (
+                            <button
+                                key={index}
+                                onClick={() => setCurrentIndex(index)}
+                                className={`w-3 h-3 sm:w-4 sm:h-4 rounded-full ${currentIndex === index ? "bg-amber-400" : "bg-emerald-400"}`}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
